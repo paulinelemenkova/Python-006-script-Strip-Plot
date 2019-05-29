956ceb0d8421882c293d835772235ba467cafaac
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-05-30 07:53:31 +0800

    Add files via upload

---
+++ Script-006-StripSedThick.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import seaborn as sb
+from matplotlib import pyplot as plt
+import pandas as pd
+import os
+os.chdir('/Users/pauline/Documents/Python')
+dfM = pd.read_csv("Tab-Morph.csv")
+sb.set_style('darkgrid')
+df = dfM.melt(id_vars=['profile'], 
+              value_vars=['plate_phill', 'plate_pacif', 'plate_maria', 'plate_carol'],
+              var_name='Plates', value_name='sedim_thick')
+#print(df.head)
+sb.stripplot(x='Plates', y='sedim_thick', hue = 'profile', palette='RdGy', data=df, jitter=True)
+plt.title('Strip plots for the sediment thickness: Mariana Trench', fontsize=10, fontfamily='serif')
+plt.legend(bbox_to_anchor=(1.14, .8), loc=1, borderaxespad=-4., ncol=2, 
+           markerscale=.5, fancybox=True, shadow=True, mode='expand', title='Bathymetric \nProfiles:',
+          title_fontsize=8, labelspacing=.4);
+plt.subplots_adjust(bottom=0.15,top=0.85, right=0.80)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
